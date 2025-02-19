e9d9aef8fb25c335cfe4ce55550cfbb127ab0751
brunotesckemartins <161394694+brunotesckemartins@users.noreply.github.com>
2025-02-19 10:39:09 -0300

    Verifica_log.sh

--- Verifica_log.sh
+++ Verifica_log.sh
@@ -2,7 +2,7 @@
 
 echo "Você está logado em $(whoami) ";
 
-if [ "$(whoami)" != "oracle" ]; then
+if [ "$(whoami)" != "log de preferêcia" ]; then
     echo "Deve estar logado em (log de preferência) para rodar esse script."
     exit
 fi
