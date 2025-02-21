ee403a66d8e65f72658b59496876875240bd8c50
brunotesckemartins <161394694+brunotesckemartins@users.noreply.github.com>
2025-02-21 07:58:04 -0300

    gerador_senha.sh

--- gerador_senha.sh
+++ gerador_senha.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gerador_senha() {
-    tr -dc 'A-Za-z0-9!@#$%^&*()_+{}' </dev/urandom | fold -w 12 | head -n 1
+    tr -dc 'A-Za-z0-9!@#$%^&*()_+{}' </dev/urandom | head -c 12
 }
 
 password=$(gerador_senha)
